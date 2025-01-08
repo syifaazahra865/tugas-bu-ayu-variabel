bb6e1b973cdd62a6e3a9abebc7bda1530e07d353
syifaazahra865 <syifaazahra865@gmail.com>
2025-01-08 11:25:20 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,20 @@
+let nama = "Syifa Azahra";
+let alamat = "Morosari Rt01/Rw05";
+let usia = "16";
+let ttl = "Demak,2 Agustus 2008";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "syifaazahra@gmail.com";
+let berat = "43kg";
+let tinggi = "155cm";
+let hobi = "memasak";
+let cita = "pengusaha muda";
+let status = "pelajar";
+let sekolah = "SMK NEGERI 1 SAYUNG";
+let jurusan = "PPLG";
+document.write(" nama saya adalah " + nama + " saya tinggal di " + alamat +
+ " pada tahun 2025 ini usia saya adalah " + usia + " saya lahir di" + ttl +
+ " jenis kelamin saya adalah " + gender + " saya beragama " + agama + " alamat email saya " + 
+ email + " saya memiliki berat badan " + berat + " dengan tinggi " + tinggi + " saya sangat senang " +
+ hobi + " karena saya memiliki cita cita " + cita + " saat ini saya adalah seorang " + student +
+ " yang bersekolah di " + sekolah + " pada jurusan " + jurusan )
